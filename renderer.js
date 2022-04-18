70b2d85255c907cf9aa0a102b49e872099ffc390
Vinit Jain <jain.vinit14@gmail.com>
2022-04-18 10:41:17 +0530

    fix Object Object on dom

--- renderer.js
+++ renderer.js
@@ -1,2 +1,4 @@
 console.log("in renderer js");
-document.getElementById("test-window-api").innerText = window.myAPI;
+document.getElementById("test-window-api").innerText = JSON.stringify(
+  window.myAPI
+);
